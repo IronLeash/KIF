034666fbd84e0155cfb2582ca9bf6e82d7fd664b
Ilker Baltaci <ilker.baltaci@sixt.com>
2014-11-19 10:49:42 +0100

    Test Change

--- Additions/LoadableCategory.h
+++ Additions/LoadableCategory.h
@@ -15,4 +15,5 @@
  *
  * @param UNIQUE_NAME A globally unique name.
  */
+
 #define MAKE_CATEGORIES_LOADABLE(UNIQUE_NAME) @interface FORCELOAD_##UNIQUE_NAME : NSObject @end @implementation FORCELOAD_##UNIQUE_NAME @end
